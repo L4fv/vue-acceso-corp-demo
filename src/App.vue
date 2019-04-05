5857d341ce93fa39ad1b731d05b8152f7213be5f
luis flores <angel.flores@acceso.com.pe>
2019-04-05 16:03:35 -0500

    add colaboradores

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,72 @@ export default {
   data() {
     return {
       // campos y datos
+      colaboradores: [
+        {
+          id: "1",
+          name: "Alfredo guzman"
+        },
+        {
+          id: "2",
+          name: "Almonacid coris "
+        },
+        {
+          id: "3",
+          name: "Baldeon sanchez "
+        },
+        {
+          id: "4",
+          name: "Bobadilla sanchez"
+        },
+        {
+          id: "5",
+          name: "Coyla mamani"
+        },
+        {
+          id: "6",
+          name: "Diaz real "
+        },
+        {
+          id: "7",
+          name: "Edgar jhonatan "
+        },
+        {
+          id: "8",
+          name: "Huari huaman "
+        },
+        {
+          id: "9",
+          name: "Huari huaman "
+        },
+        {
+          id: "10",
+          name: "Huillca javier"
+        },
+        {
+          id: "11",
+          name: "Javier javier "
+        },
+        {
+          id: "12",
+          name: "Maravi salcedo"
+        },
+        {
+          id: "13",
+          name: "Maria del pilar "
+        },
+        {
+          id: "14",
+          name: "Nuñez rivas "
+        },
+        {
+          id: "15",
+          name: "Silva malca "
+        },
+        {
+          id: "16",
+          name: "william loayza"
+        }
+      ]
     };
   },
   methods: {
