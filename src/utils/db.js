0136f7bac1ac315027704cb2d5f979d1ca517f88
luis flores <angel.flores@acceso.com.pe>
2019-04-05 17:12:50 -0500

    update bd

---
+++ src/utils/db.js
@@ -0,0 +1,84 @@
+export const productos = [
+  {
+    id: "1",
+    detalle: "pan con pollo",
+    price: "4"
+  },
+  {
+    id: "2",
+    detalle: "galletas",
+    price: "0.6"
+  },
+  {
+    id: "3",
+    detalle: "chocolate",
+    price: "1.1"
+  }
+];
+
+export const colaboradores = [
+  {
+    id: "1",
+    name: "Alfredo guzman"
+  },
+  {
+    id: "2",
+    name: "Almonacid coris"
+  },
+  {
+    id: "3",
+    name: "Baldeon sanchez"
+  },
+  {
+    id: "4",
+    name: "Bobadilla sanchez"
+  },
+  {
+    id: "5",
+    name: "Coyla mamani"
+  },
+  {
+    id: "6",
+    name: "Diaz real"
+  },
+  {
+    id: "7",
+    name: "Edgar jhonatan"
+  },
+  {
+    id: "8",
+    name: "Huari huaman"
+  },
+  {
+    id: "9",
+    name: "Huari huaman"
+  },
+  {
+    id: "10",
+    name: "Huillca javier"
+  },
+  {
+    id: "11",
+    name: "Javier javier"
+  },
+  {
+    id: "12",
+    name: "Maravi salcedo"
+  },
+  {
+    id: "13",
+    name: "Maria del pilar"
+  },
+  {
+    id: "14",
+    name: "Nuñez rivas"
+  },
+  {
+    id: "15",
+    name: "Silva malca"
+  },
+  {
+    id: "16",
+    name: "william loayza"
+  }
+];
