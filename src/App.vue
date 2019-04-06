6bc50481b38b1be32a808092c9cbf37974789b97
luis flores <angel.flores@acceso.com.pe>
2019-04-05 21:00:38 -0500

    responsive design

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,44 @@
       </v-toolbar-title>
     </v-toolbar>
     <v-content class="ma-4">
-      <!-- star here -->       
-      <v-select :items="colaboradores" item-text="name" v-model="persona"
-       item-value="id" label="Lista de colaboradores" return-object></v-select>       
-       <v-select :items="productos" item-text="detalle" v-model="producto"
-       item-value="id" label="Lista de Productos" return-object></v-select>
-       <v-select :items="[1,2,3,4,5]" v-model="unidades"></v-select>
-       <v-btn dark @click="agregarItem">agregar</v-btn>
-       <v-layout wrap row v-for="(item, key) in reporteDiario" 
-        :key="key">
-         <v-flex xs3 class="red">{{item.nombre}}</v-flex>
-         <v-flex xs4 class="blue">{{item.detalle}}</v-flex>
-         <v-flex xs3 class="yellow">{{item.price}}</v-flex>
-         <v-flex xs2 class="yellow">
-           <v-btn @click="eliminarItem(item)" icon>
-             <v-icon>delete</v-icon></v-btn></v-flex>
-       </v-layout>
+      <!-- star here -->
+      <v-layout wrap row>
+        <v-flex xs12 sm4 pa-2>
+          <v-select
+            :items="colaboradores"
+            item-text="name"
+            v-model="persona"
+            item-value="id"
+            label="Lista de colaboradores"
+            return-object
+          ></v-select>
+        </v-flex>
+        <v-flex xs12 sm4 pa-2>
+          <v-select
+            :items="productos"
+            item-text="detalle"
+            v-model="producto"
+            item-value="id"
+            label="Lista de Productos"
+            return-object
+          ></v-select>
+        </v-flex>
+        <v-flex xs12 sm4 pa-2>
+          <v-select :items="[1,2,3,4,5]" v-model="unidades"></v-select>
+        </v-flex>
+      </v-layout>
+
+      <v-btn dark @click="agregarItem">agregar</v-btn>
+      <v-layout wrap row v-for="(item, key) in reporteDiario" :key="key">
+        <v-flex xs3 class="red">{{item.nombre}}</v-flex>
+        <v-flex xs4 class="blue">{{item.detalle}}</v-flex>
+        <v-flex xs3 class="yellow">{{item.price}}</v-flex>
+        <v-flex xs2 class="yellow">
+          <v-btn @click="eliminarItem(item)" icon>
+            <v-icon>delete</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
     </v-content>
   </v-app>
 </template>
@@ -31,25 +53,28 @@
 import { colaboradores, productos } from "./utils/db.js";
 export default {
   name: "App",
-   methods: {
-     eliminarItem(item){
-       this.reporteDiario.splice(item, 1)
-     },
-    agregarItem(){
-      this.reporteDiario.push(
-        {nombre: this.persona.name,detalle:this.producto.detalle,
-         price:this.producto.price }) }  },
+  methods: {
+    eliminarItem(item) {
+      this.reporteDiario.splice(item, 1);
+    },
+    agregarItem() {
+      this.reporteDiario.push({
+        nombre: this.persona.name,
+        detalle: this.producto.detalle,
+        price: this.producto.price
+      });
+    }
+  },
   data() {
     return {
-      unidades:1,
+      unidades: 1,
       reporteDiario: [],
       nombre: null,
-      persona:{name:null},
-      producto:{detalle:null},
+      persona: { name: null },
+      producto: { detalle: null },
       colaboradores: colaboradores,
       productos: productos
     };
-  },
- 
+  }
 };
 </script>
