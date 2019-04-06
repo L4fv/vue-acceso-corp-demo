3cf0f3b81b95960dd7a600aae4fd7e52c10e3e95
luis flores <angel.flores@acceso.com.pe>
2019-04-05 20:35:56 -0500

    update array

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,19 @@
       </v-toolbar-title>
     </v-toolbar>
     <v-content class="ma-4">
-      <!-- star here -->
-      <v-text-field label="Nombre" v-model="nombre"></v-text-field>      
+      <!-- star here -->       
       <v-select :items="colaboradores" item-text="name" v-model="persona"
        item-value="id" label="Lista de colaboradores" return-object></v-select>       
        <v-select :items="productos" item-text="detalle" v-model="producto"
        item-value="id" label="Lista de Productos" return-object></v-select>
+       <v-select :items="[1,2,3,4,5]" v-model="unidades"></v-select>
        <v-btn dark @click="agregarItem">agregar</v-btn>
-       {{reporteDiario}}
+       <v-layout wrap row v-for="(item, key) in reporteDiario" 
+        :key="key">
+         <v-flex xs4 class="red">{{item.nombre}}</v-flex>
+         <v-flex xs4 class="blue">{{item.detalle}}</v-flex>
+         <v-flex xs4 class="yellow">{{item.price}}</v-flex>
+       </v-layout>
     </v-content>
   </v-app>
 </template>
@@ -25,12 +30,13 @@ export default {
   name: "App",
    methods: {
     agregarItem(){
-      this.reporteDiario.push("hola")
-    }
-    // funciones
-  },
+      this.reporteDiario.push(
+        {nombre: this.persona.name,
+         detalle:this.producto.detalle,
+         price:this.producto.price }) }  },
   data() {
     return {
+      unidades:1,
       reporteDiario: [],
       nombre: null,
       persona:{name:null},
