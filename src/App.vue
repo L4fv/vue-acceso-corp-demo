c4192b6d270b9d02fc30a9303d314968c2319037
luis flores <angel.flores@acceso.com.pe>
2019-04-05 20:14:04 -0500

    class 1

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,15 @@
         <span class="font-weight-light blue--text">ACCESO</span>
       </v-toolbar-title>
     </v-toolbar>
-    <v-content>
+    <v-content class="ma-4">
       <!-- star here -->
+      <v-text-field label="Nombre" v-model="nombre"></v-text-field>      
+      <v-select :items="colaboradores" item-text="name" v-model="persona"
+       item-value="id" label="Lista de colaboradores" return-object></v-select>       
+       <v-select :items="productos" item-text="detalle" v-model="producto"
+       item-value="id" label="Lista de Productos" return-object></v-select>
+       <v-btn dark @click="agregarItem">agregar</v-btn>
+       {{reporteDiario}}
     </v-content>
   </v-app>
 </template>
@@ -16,15 +23,22 @@
 import { colaboradores, productos } from "./utils/db.js";
 export default {
   name: "App",
+   methods: {
+    agregarItem(){
+      this.reporteDiario.push("hola")
+    }
+    // funciones
+  },
   data() {
     return {
-      // campos y datos
+      reporteDiario: [],
+      nombre: null,
+      persona:{name:null},
+      producto:{detalle:null},
       colaboradores: colaboradores,
       productos: productos
     };
   },
-  methods: { 
-    // funciones
-  }
+ 
 };
 </script>
